aedf351eabb86b0ed1f13c652cdabe6cc92dd8b1
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-05-29 23:16:50 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,10 @@
+def kattasini_chiqaruvchi(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+a = int(input("Birinchi sonni kiriting: "))
+b = int(input("Ikkinchi sonni kiriting: "))
+
+print(kattasini_chiqaruvchi(a, b))
